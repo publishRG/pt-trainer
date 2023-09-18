322175ba9a0122a79680c8cd62210eae868fea35
publishRG <144844165+publishRG@users.noreply.github.com>
2023-09-18 17:58:56 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import WhatsAppButton from "./components/WhatsappButton";
+import Navbar from "./components/Navbar"; // Assicurati che il percorso sia corretto
+import Home from "./components/Home"; // Importa il componente per la Home
+import Servizi from "./components/Servizi"; // Importa il componente per i Servizi
+import Chisono from "./components/ChiSono"; // Importa il componente per Chi Sono
+import Footer from "./components/Footer";
+
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/servizi" element={<Servizi />} />
+        <Route path="/chisono" element={<Chisono />} />
+        {/* Utilizza un Route diverso per la sezione Contatti, in base alle tue esigenze */}
+      </Routes>
+      <WhatsAppButton phoneNumber="+393934361743" />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
