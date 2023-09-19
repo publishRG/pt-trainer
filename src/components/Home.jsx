21e4c29692dad8cde782ca0365a2a0659d0e3806
publishRG <144844165+publishRG@users.noreply.github.com>
2023-09-19 19:28:01 +0200

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,9 +6,6 @@ import CardPricing from "./Card";
 import ImageCompSlider from "./ImageCompSlider";
 import VideoGrid from "./VideoGrid";
 
-
-
-
 import ca1 from "./ca1.png";
 import ca2 from "./ca2.png";
 import cam3 from "./cam3.png";
@@ -26,7 +23,12 @@ const Home = () => {
     maxHeight: "80px", // Imposta l'altezza massima desiderata per le immagini
   };
 
- const videoArray = [{ src: https://drive.google.com/file/d/1TPfcSvHep6TX6PVt5R65w-7hImZN3l2Y/view?usp=drive_link }, { src: https://drive.google.com/file/d/1NerdQgPuE5L6lZA8QcAkGCxhVCci_gUq/view?usp=drive_link }, { src: https://drive.google.com/file/d/18UYQfkrf9JpBC5Vq0ljWSzBu91mi-KJx/view?usp=drive_link }, { src: https://drive.google.com/file/d/1PIBinbHddpLnTCuMIm0oEjl-oS8ADcqi/view?usp=drive_link }];
+  const videoArray = [
+    { src: "https://drive.google.com/file/d/1TPfcSvHep6TX6PVt5R65w-7hImZN3l2Y/view?usp=drive_link" },
+    { src: "https://drive.google.com/file/d/1NerdQgPuE5L6lZA8QcAkGCxhVCci_gUq/view?usp=drive_link" },
+    { src: "https://drive.google.com/file/d/18UYQfkrf9JpBC5Vq0ljWSzBu91mi-KJx/view?usp=drive_link" },
+    { src: "https://drive.google.com/file/d/1PIBinbHddpLnTCuMIm0oEjl-oS8ADcqi/view?usp=drive_link" }
+  ];
 
   return (
     <div className="home-container">
@@ -56,7 +58,7 @@ const Home = () => {
           Storie di <span>successo</span>
         </h1>
       </div>
-       <VideoGrid videos={videoArray} />
+      <VideoGrid videos={videoArray} />
       <div className="additional-info">
         <p>
           <span>+</span> 12.320 ore di personal training
